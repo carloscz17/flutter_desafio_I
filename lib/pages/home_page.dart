507907df675adf160f77a84044a9b80713a9e8ce
carloscz17 <carloswarface5@gmail.com>
2024-06-04 14:15:57 -0300

    Adicionando o modo gaveta

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,80 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final List<String> imgList = [
     'lib/assets/image_carrosel.png',
     'lib/assets/image_carrosel.png',
     'lib/assets/image_carrosel.png',
   ];
+
+  void _showModalBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'label modal gaveta',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Nalu lorem molta cupsiajr iurce. Lubauma nyr euis udis uditomo celum dor.',
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('CLIQUE PARA FECHAR'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(16.0),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text(
+                'label modal overlay',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Nalu lorem molta cupsiajr iurce. Lubauma nyr euis udis uditomo celum dor.',
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('CLIQUE PARA FECHAR'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +91,7 @@ class MyHomePage extends StatelessWidget {
               children: [
                 CircleAvatar(
                   radius: 30,
-                  backgroundImage: AssetImage('lib/assets/profile.png'), 
+                  backgroundImage: AssetImage('lib/assets/profile.png'),
                 ),
                 SizedBox(width: 10),
                 Text(
@@ -32,11 +101,16 @@ class MyHomePage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 20),
-            Container(
-              color: Colors.grey[300],
-              height: 100,
-              width: double.infinity,
-              child: Center(child: Text('container fixo')),
+            GestureDetector(
+              onTap: () {
+                _showDialog(context);
+              },
+              child: Container(
+                color: Colors.grey[300],
+                height: 100,
+                width: double.infinity,
+                child: Center(child: Text('container fixo')),
+              ),
             ),
             const SizedBox(height: 20),
             const Text(
@@ -49,14 +123,20 @@ class MyHomePage extends StatelessWidget {
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: imgList.map((item) {
-                  return Container(
-                    width: 150,
-                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                    ),
-                    child: const Center(
-                      child: Icon(Icons.play_circle_outline, color: Colors.white, size: 50),
+                  return GestureDetector(
+                    onTap: () {
+                      _showModalBottomSheet(context);
+                    },
+                    child: Container(
+                      width: 150,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                      ),
+                      child: const Center(
+                        child: Icon(Icons.play_circle_outline,
+                            color: Colors.white, size: 50),
+                      ),
                     ),
                   );
                 }).toList(),
